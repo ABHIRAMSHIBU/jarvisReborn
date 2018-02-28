148b31abd4561e84064a5acd173250e14cb0ea22
Abhiram Shibu <abhiramshibu1998@gmail.com>
2018-02-28 06:40:37 +0530

    Jarvis: Added splash!

--- src/Core.java
+++ src/Core.java
@@ -1,47 +1,23 @@
-import org.telegram.telegrambots.*;
-import org.telegram.telegrambots.api.methods.send.SendMessage;
-import org.telegram.telegrambots.api.objects.Update;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.exceptions.*;
-//import org.telegram.telegrambots..*;
-class MyAmazingBot extends TelegramLongPollingBot {
-    @Override
-    public void onUpdateReceived(Update update) {
-    	if (update.hasMessage() && update.getMessage().hasText()) {
-            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-                    .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
-    @Override
-    public String getBotUsername() {
-        return "Jarvis_telegram_bot";
-    }
-
-    @Override
-    public String getBotToken() {
-        // TODO
-        return "453584242:AAGWcM0iJr9wUGrOYEoBtzFsKlZRKT39sSQ";
-    }
-}
 public class Core {
 	public static void main(String[] args) {
 		new Core();
-		ApiContextInitializer.init();
-        TelegramBotsApi botsApi = new TelegramBotsApi();
-        try {
-        	botsApi.registerBot(new MyAmazingBot());
-	    } catch (TelegramApiException e) {
-	    	e.printStackTrace();
-	    }
-	}
+	}	
 	Core(){
-		new GUI();
+		//Creating gui thread
+		Thread gui = new Thread() {
+			public void run() {
+				new GUI();
+			}
+		};
+		//Creating bot thread
+		Thread bot = new Thread() {
+			public void run() {
+				new TelegramBot();
+			}
+		};
+		/* Start both threads */
+		gui.start();
+		bot.start();
 	}
 }
--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,7 @@
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Image;
+import java.net.MalformedURLException;
 import javax.swing.*;
 public class GUI {
 	/* All critical GUI Components will go here */
@@ -31,7 +35,27 @@ public class GUI {
 	void kill(Gui g) {
 		g.f.dispose();
 	}
-	Gui initGUI() {
+	void splash() {
+		GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		int width = gd.getDisplayMode().getWidth();
+		int height = gd.getDisplayMode().getHeight();
+		String imageDir="/home/abhiram/eclipse-workspace/JarvisReborn/src/splash.jpg";
+		JWindow window = new JWindow();
+		window.getContentPane().add(new JLabel("", new ImageIcon(new ImageIcon(imageDir).getImage().getScaledInstance(width/2, height/2, Image.SCALE_DEFAULT)), SwingConstants.CENTER));
+		window.setBounds(500, 150, 300, 200);
+		window.setVisible(true);
+		try {
+		    Thread.sleep(5000);
+		} catch (InterruptedException e) {
+		    e.printStackTrace();
+		}
+		window.setVisible(false);
+		window.dispose();
+		
+	}
+	Gui initGUI() throws MalformedURLException {
+		
+		splash();
 		Gui g1=new Gui();
 		g1.f=new JFrame();
 		g1.f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -40,10 +64,15 @@ public class GUI {
 		return g1;
 	}
 	GUI(){
-		Gui g1 = initGUI();
+		Gui g1=null;
+		try {
+			g1 = initGUI();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		JLabel title = new JLabel("Welcome to jarvis!");
-		title.setBounds(0,0,g1.x,40);
-		title.setHorizontalAlignment(JLabel.CENTER);
+		title.setBounds(0, 0, 0, 0);
 		g1.f.add(title);
 	}
 	
