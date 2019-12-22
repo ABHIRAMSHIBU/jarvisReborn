ec13969f65bce5cde6f983ab21896be5f5158b4c
Abhiram <abhiramshibu1998@gmail.com>
2019-12-22 20:39:32 +0530

    Simulator fully functional, added pip
    Special thanks to @abhijithnraj

--- ssal_core_simulator.py
+++ ssal_core_simulator.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python
 from ssal_core_simulator_main import *
 d=Device("0",0.2,0.1)
-def handle_client(conn,addr,id):
+def handle_client(conn,addr,ID):
+    global id
     print("Waiting client thread")
     while(True):
         n=conn.recv(100)
-        if(id is not 0):
+        if(ID != 0):
             conn.send(b"Not Allowed!\r\n")
         else:
             if(n==b""):
                 break
-            print(n.decode(),end="")
-            processedOutput=str(d.recieve(n.decode().strip()))
-            print(processedOutput)
-            conn.send(((processedOutput)+"\r\n").encode())
-            print("ID=",id)
+            try:
+                print(n.decode(),end="")
+                processedOutput=str(d.recieve(n.decode().strip()))
+                print(processedOutput)
+                conn.send(((processedOutput)+"\r\n").encode())
+                print("ID=",ID)
+            except UnicodeDecodeError:
+                print("decode error handled")
+    if(ID==0):
+        pwf.write("idzf")
+        pwrf.flush()
+
+
 
 import socket
 import sys
 import os
 
 HOST = '0.0.0.0'	# Symbolic name, meaning all available interfaces
-PORT = 7000	# Arbitrary non-privileged port
+PORT = 23	# Arbitrary non-privileged port
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Socket created')
@@ -41,13 +50,22 @@ s.listen(10)
 print('Socket now listening')
 
 #now keep talking with the client
+id=0  #set global id
+pr,pw=os.pipe()
+import fcntl
+fcntl.fcntl(pr, fcntl.F_SETFL, os.O_NONBLOCK) 
+prf=os.fdopen(pr,"r")
+pwf=os.fdopen(pw,"w")
 try:
-    id=0
     while 1:
         #wait to accept a connection - blocking call
         conn, addr = s.accept()
+        z=prf.read(4)
+        if(z=="idzf"):
+            id=0
+        print("Read z value pipe : ",z)
         pid=os.fork()
-        if(pid is not 0):
+        if(pid !=  0):
             print('Connected with ' + addr[0] + ':' + str(addr[1]))
             print("Parent continuing")
             id+=1
