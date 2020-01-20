24015107286f3a977e0d42ac93540d6567fbfb6a
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-01-20 10:50:09 +0530

    Added client, added name to docker container

---
+++ get-shell.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Copyleft 2020 Abhiram Shibu
+# Project SSAL-OS
+sudo docker exec -it db influx
+echo "Bye.."
+
--- start.sh
+++ start.sh
@@ -3,5 +3,10 @@
 # Project SSAL-OS
 echo "Project SSAL-OS docker container for influxdb is starting.. Please Wait.."
 echo "Port 8086 and 8083 will be listning"
-sudo docker run -p 8086:8086 -p 8083:8083 -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro -v $PWD/database:/var/lib/influxdb influxdb -config /etc/influxdb/influxdb.conf
+sudo docker run -p 8086:8086 -p 8083:8083 -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro -v $PWD/influx.log:/var/log/influx.log -v $PWD/runCliDaemon.sh:/runCliDaemon.sh -v $PWD/database:/var/lib/influxdb --name "db" influxdb -config /etc/influxdb/influxdb.conf
+if[ $? -ne 0 ] then
+	echo "Some problem occured"
+else
+	echo "Everything was as expected"
+fi
 echo "Docker container has quit"
