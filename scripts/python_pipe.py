d498e7967c34ab564d0236fcacfd977d4b5fc2b6
abhijithnraj <abhijithnrajg@gmail.com>
2020-01-27 16:35:04 +0530

    python pipe code for EFPS

--- scripts/python_pipe.py
+++ scripts/python_pipe.py
@@ -2,5 +2,32 @@ import os
 PIPE_DIRECTORY = "/run/user/"+str(os.getuid())+"/ssald"
 if(not os.path.exists(PIPE_DIRECTORY)):
     os.mkdir(PIPE_DIRECTORY)
-PIPE_FILE = PIPE_DIRECTORY+"/in"
-os.mkfifo(PIPE_FILE)
+PYTHON_PIPE_FILE = PIPE_DIRECTORY+"/in"
+JAVA_PIPE_FILE = PIPE_DIRECTORY+"/out"
+try:
+    os.mkfifo(PYTHON_PIPE_FILE)
+except:
+    pass
+try:
+    os.mkfifo(JAVA_PIPE_FILE)
+except:
+    pass
+
+fin = open(JAVA_PIPE_FILE,"r")
+fout = open(PYTHON_PIPE_FILE,"w")
+try:
+    while True:
+        fout.print("Hello")
+        fout.flush()
+        while(True):
+            z = fin.read()
+            if(z==""):
+                continue
+            else:
+                print(z)
+                break
+except KeyboardInterrupt:
+    fin.close()
+    fout.close()
+    os.remove(PYTHON_PIPE_FILE)
+    os.remove(JAVA_PIPE_FILE)
