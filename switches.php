c9040a9721d8b75e4aa41ff1c043be6dcee2ffee
harikrishnan <harikrishnan310798@gmail.com>
2020-02-12 14:49:53 +0530

    reverted switches.php

--- switches.php
+++ switches.php
@@ -3,6 +3,147 @@
 <?php require 'auth.php' ?>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
+.popup{
+    width:100%;
+    height:100%;
+    opacity:.95;
+    display:none;
+    position:fixed;
+    background-color:#313131;
+    overflow:auto;
+    font-color:white;
+}
+.inpopup{
+    font-size:40px;
+    text-align:center;
+    color:white;
+}
+.button {
+  display: inline-block;
+  border-radius: 4px;
+  background-color: #f4511e;
+  border: none;
+  color: #FFFFFF;
+  text-align: center;
+  font-size: 28px;
+  padding: 20px;
+  width: 200px;
+  transition: all 0.5s;
+  cursor: pointer;
+  margin: 5px;
+}
+
+.button span {
+  cursor: pointer;
+  display: inline-block;
+  position: relative;
+  transition: 0.5s;
+}
+
+.button span:after {
+  content: '\00bb';
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  right: -20px;
+  transition: 0.5s;
+}
+
+.button:hover span {
+  padding-right: 25px;
+}
+.button:hover{
+    color:green;
+}
+.button:hover span:after {
+  opacity: 1;
+  right: 0;
+}
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {display:none;}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  -webkit-transition: .4s;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #2196F3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(26px);
+  -ms-transform: translateX(26px);
+  transform: translateX(26px);
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+.center {
+    text-align:center;
+}
+.addbutton{
+    position:fixed;
+    right:5%;
+    bottom:5%;
+}
+.rembutton{
+    position:fixed;
+    left:5%;
+    bottom:5%;
+}
+.roundbutton {
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+}
+h2{
+    text-align:center;   
+    font-size:40px;
+}
+hr{
+    color:lightgreen;
+    background: lightgreen;
+    height: 2px;
+}
+body{
+    font-family: Avantgarde, TeX Gyre Adventor, URW Gothic L, sans-serif;
+}
 </style>
 	<script>
         if(typeof(String.prototype.trim) === "undefined")
@@ -129,7 +270,6 @@
     startRequest();
     setInterval(startRequest,500);
 	</script>
-	<link rel="stylesheet" href="css/styles.css">
 </head>
 <body bgcolor="black">
 	<div class=popup id=popup hidden=true>
@@ -139,6 +279,21 @@
     <font color="white">
     <h2>Toggle Switch</h2>
     </font>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!--<div class="navbar-header">
+                <a class="navbar-brand" href="#">WebSiteName</a>
+            </div>-->
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="index.php">Home</a></li>
+<!--             <li><a href="powerpanel.php">Power panel</a></li> -->
+        </ul>
+        </div>
+        <form action="powerpanel.php" method="POST" >
+            <input type=hidden name="id" value="<?php echo $devID; ?>">
+            <input type="submit" value="Powerpanel">
+        </form>
+    </nav>
     <form target="_blank" action="handle.php" method="post" id="forms">
 	    <?php
 	    echo "<center><hr>";
@@ -179,23 +334,6 @@
 	        echo "</td>";
 	        $i++;
 	    }
-	    
-	    
-// 	    for($i=0;$i<10;$i++){
-// 	        if($i%2==0){
-// 	            if(!$start){
-// 	                echo "</tr>";
-// 	            }
-// 	            echo "<tr>";
-// 	        }
-// 	        echo "<td>";
-//             echo "<span style='color: white; font-size: 45px;'>Pin $i\t:\t</span>";
-//             echo '<label class="switch">';
-//             echo "<input type='checkbox' name='$i' />";
-//             echo '<span class="slider round"></span>';
-//             echo '</label>';
-//             echo "</td>";
-//         }
         echo "</tr>";
         echo "</table>";
         echo "</center>"
