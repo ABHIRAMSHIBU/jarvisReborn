4cda143f7693853c7217d991e80274772e0f30b6
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-03-22 16:54:06 +0530

    RASA: Get asyncio loop from caller

--- bot/rasahandler.py
+++ bot/rasahandler.py
@@ -1,11 +1,12 @@
 class chatbot:
-    def __init__(self):
+    def __init__(self,loop):
         from rasa_nlu.model import Interpreter
         from rasa.core.agent import Agent
         from rasa.core.interpreter import RasaNLUInterpreter
         import os
         import asyncio as asyncio
         self.asyncio=asyncio
+        self.loop=loop
         # os.chdir("chatbot")
         model_path = "chatbot/models/current"
         self.agent = Agent.load(model_path)
@@ -13,7 +14,8 @@ class chatbot:
     def predict_nlu(self,string):
         return self.interpreter.parse(string)
     def predict_chat(self,string):
-        return self.asyncio.run(self.agent.handle_text(string))
+        loop = self.loop
+        return loop.run_until_complete(self.agent.handle_text(string))
 # Chat bot test code
 # obj=chatbot()
 # try:
