e88b1e2c7bcbedcf771f79ef4ab710b162b81b71
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-04-19 11:18:04 +0530

    SSAL Core: Added commands for DOS, Export to EEPROM Added
    Now hardware is DOS ready.
    Commands added
    relaycfg <relayNo> <relayConfigurationInDec>
    serrelay <relayNo> <DOSState>
    
    DOSState
    0-> Disconnect
    1-> Connected to grid
    2-> Connected to alternative power source
    
    relayConfiguration Example
    00000011 -> 3
    10000001 -> 127
    
    Co-authored-by: Abhijith N Raj <abhijithnrajg@gmail.com>

--- stm32_ESP_CONTROLLER.ino
+++ stm32_ESP_CONTROLLER.ino
@@ -65,13 +65,15 @@
 
 ---End ADC Sensor Map    */
 //LCD VARIABLES
-    int count=0;
-    int count_wait=0;
-    int pinNow=0;
-    long time1=millis();     //LCD Scroll timer line 1
-    long time2=millis();     //LCD Scroll timer line 2
-    int LCD_AVAIL;
-    int LCD_UPDATE_ENABLE=true;
+    //count[0], count
+    //count[1], count_wait
+    short count[2]={0,0};
+    short pinNow=0;
+    //time1[0], time1 // LCD Scroll timer line 1
+    //time1[1], time2 // LCD Scroll timer line 2
+    long time1[2]={millis(),millis()};
+    bool LCD_AVAIL;
+    bool LCD_UPDATE_ENABLE=true;
     LiquidCrystal_I2C *lcd;
 //END LCD VARIABLES
 //SENSOR VARIABLES
@@ -82,8 +84,9 @@
 //END SENSOR VARIABLES
 //ESP VARIABLES    
 bool checkESP=true;
-int counter=0;
-int counter_disconnect=0;
+// counter[0], counter
+// counter[1], counter_disconnect
+short counter[2]={0,0};
 //END ESP VARIABLES
 //HEART BEAT VARIABLE
 bool led=False;
@@ -91,12 +94,133 @@ long time=millis();          //Heart beat timer
 //END HEART BEAT VARIABLE
 //DOS VARIABLES
 bool DOS=0;
+uint8_t relaycfg[4];
 //END DOS VARIABLES
 bool pins[16];    // Pin stats are stored here
 float temp;
 /** EEPROM BEGIN **/
+
+
+/** Relay Map BEGIN **/
+short relayMaptoInt(uint8_t input){
+    uint8_t out=1;
+    if(input==0b00000000){
+        return 0;
+    }
+    short count=0;
+    for(uint8_t i=1;i<8;i++){
+        count++;
+        out=pow(2,i)+1;
+        while((out & 0b10000000)!=0b10000000){
+            if(out==input){
+                return count;
+            }
+            count++;
+            out=out<<1;
+        }
+        if(out==input){
+            return count;
+        }
+    }
+    return -1;
+}
+uint8_t inttoRelayMap(short input){
+    uint8_t out=1;
+    if(input==0){
+        return 0b00000000;
+    }
+    short count=0;
+    for(uint8_t i=1;i<8;i++){
+        count++;
+        out=pow(2,i)+1;
+        while((out & 0b10000000)!=0b10000000){
+            if(count==input){
+                return out;
+            }
+            count++;
+            out=out<<1;
+        }
+        if(count==input){
+            return out;
+        }
+    }
+    return 0b11111111;
+}
+
+//Needs to be appened some missing code
+void relayConfLoadEEPROM(){
+    uint32_t temp = EEPROM.read(1) | EEPROM.read(2) << 8 | EEPROM.read(3) << 16;
+    short r1 = temp & 31;
+    short r2 = (temp & 992) >> 5;
+    short r3 = (temp & 31744) >> 10;
+    short r4 = (temp & 1015808) >> 15;
+    relaycfg[0]=inttoRelayMap(r1);
+    relaycfg[1]=inttoRelayMap(r2);
+    relaycfg[2]=inttoRelayMap(r3);
+    relaycfg[3]=inttoRelayMap(r4);
+    Serial.println(F("EEPROM_RELAYCONFL"));
+    Serial.println(relaycfg[0]);
+    Serial.println(relaycfg[1]);
+    Serial.println(relaycfg[2]);
+    Serial.println(relaycfg[3]);
+    //std::cout<<std::bitset<8>(inttoRelayMap(r1))<<std::endl;
+    //std::cout<<std::bitset<8>(inttoRelayMap(r2))<<std::endl;
+    //std::cout<<std::bitset<8>(inttoRelayMap(r3))<<std::endl;
+    //std::cout<<std::bitset<8>(inttoRelayMap(r4))<<std::endl;
+}
+void relayConfDumpEEPROM(){
+    uint8_t out[3];
+    short r1=relayMaptoInt(relaycfg[0]),r2=relayMaptoInt(relaycfg[1]),r3=relayMaptoInt(relaycfg[2]),r4=relayMaptoInt(relaycfg[3]);
+    // std::cout<<r1<<" "<<unsigned(relayconf.r2)<<std::endl;
+    uint32_t temp=r1 | r2<<5 | r3 << 10 | r4 <<15;
+    Serial.println(temp,BIN);
+    Serial.println();
+    // std::cout<<std::bitset<32>(temp)<<std::endl;
+    out[0]= 255 & temp;
+    out[1]= (65280 & temp)>>8;
+    out[2]= (16711680 & temp)>>16;
+    //std::cout<<std::bitset<8>(out[0])<<std::endl;
+    //std::cout<<std::bitset<8>(out[1])<<std::endl;
+    //std::cout<<std::bitset<8>(out[2])<<std::endl;
+    //relayConfLoadEEPROM(out);
+    EEPROM.write(1,out[0]);
+    EEPROM.write(2,out[1]);
+    EEPROM.write(3,out[2]);
+    Serial.println(F("EEPROM_RELAYCONFD"));
+    Serial.println(relaycfg[0]);
+    Serial.println(relaycfg[1]);
+    Serial.println(relaycfg[2]);
+    Serial.println(relaycfg[3]);
+    Serial.println();
+    Serial.println(out[0]);
+    Serial.println(out[1]);
+    Serial.println(out[2]);
+}
+
+bool relayConfDecode(uint8_t relay,short * pin1, short * pin2){
+  //Bigger pin number turns off and on a device (master switch)
+  //Smaller pin number switches grid
+  short count=0,pos[2]={0,0},j=0;
+  while(relay!=0){
+    if((relay&1) == 1){
+      if(j==3){
+        return 0;
+      }
+      pos[j]=count;
+      j++;
+    }
+    relay=relay>>1;
+    count++;
+  }
+  *(pin1)=2+pos[0];
+  *(pin2)=2+pos[1];
+  return 1;
+}
+/** END RelayMap **/
+
 bool PROGMEM EEPROMFormat=False;
 void EEPROMinit(){
+    //Old value 0x800F000, 0x800F400
     EEPROM.PageBase0=0x800F000;
     EEPROM.PageBase1=0x800F400;
     if(EEPROMFormat==True){
@@ -235,8 +359,8 @@ String pinData(){
     else{
         temp+=" is on";
     }
-    if((millis()-time2)>500){
-        time2=millis();
+    if((millis()-time1[1])>500){
+        time1[1]=millis();
         if(pinNow++==13){
             pinNow=0;
         }
@@ -258,25 +382,25 @@ void updateLCD(){
     if(LCD_UPDATE_ENABLE==false){
         return; // no need to update
     }
-	if((millis()-time1)>300){ //Scroll and set if update time exceeds..
-		time1=millis();
+	if((millis()-time1[0])>300){ //Scroll and set if update time exceeds..
+		time1[0]=millis();
 		String main_message=assembleMessage();
 		String temp_message;
     temp_message.reserve(100);
 		temp_message=String(main_message);
-		String t=temp_message.substring(0,count);
-		temp_message.remove(0,count);
+		String t=temp_message.substring(0,count[0]);
+		temp_message.remove(0,count[0]);
 		temp_message=temp_message+t;
 		lcd->setCursor(0,0);
 		lcd->print(temp_message.substring(0,16));
-		if(count==0&&count_wait<10){
-			count_wait++;
+		if(count[0]==0&&count[1]<10){
+			count[1]++;
 		}
 		else{
-			count_wait=0;
-			count++;
-			if(count==main_message.length()){
-				count=0;
+			count[1]=0;
+			count[0]++;
+			if(count[0]==main_message.length()){
+				count[0]=0;
 			}
 		}
 	}
@@ -342,8 +466,8 @@ void initializeESP(){
     /* REST ESP ON UNKNOWN STATE */
     if(data.indexOf(F("ERROR"))>-1){
       if(data.indexOf(F("builded"))>-1){
-        counter++;
-        if(counter==3){
+        counter[0]++;
+        if(counter[0]==3){
           writeESP(F("AT+RST\r\n"));
           serialClear();
         }
@@ -358,8 +482,8 @@ void initializeESP(){
     /* REST ESP ON UNKNOWN STATE */
     if(data.indexOf(F("ERROR"))>-1){
       if(data.indexOf(F("builded"))>-1){
-        counter++;
-        if(counter==3){
+        counter[0]++;
+        if(counter[0]==3){
           writeESP(F("AT+RST\r\n"));
           serialClear();
         }
@@ -369,7 +493,7 @@ void initializeESP(){
         Serial.print(F("Link Active"));
       }
     }
-    counter=0;
+    counter[0]=0;
 }
 /* ESP sendData function for automatically doing all sending procedure */
 void sendData(int id, String pinString){
@@ -472,6 +596,12 @@ int pinmap(int x){
     // PB10 and PB11 is used for i2c
     return pin[x];
 }
+void setpin(short pin,bool operation){
+  pinMode(pinmap(pin),OUTPUT);
+  digitalWrite(pinmap(pin),operation);
+  pins[pin]=operation;
+  dumpToEEPROM();
+}
 /* END pinmap */
 
 /* pin initialization */
@@ -500,6 +630,7 @@ void setup() {
   initLCD();
   EEPROMinit();
   loadFromEEPROM();
+  relayConfLoadEEPROM();
   pinInit();
   if(DEBUG){
     Serial.begin(BAUD); //Serial to debugger 0,1
@@ -541,9 +672,9 @@ void loop() {
     Serial.print(F("Hotspot data:"));
     Serial.println(data);
     if(data.indexOf(F(":5"))>-1){
-        counter_disconnect++;
-        if(counter_disconnect==3){
-            counter_disconnect=0;
+        counter[1]++;
+        if(counter[1]==3){
+            counter[1]=0;
             writeESP(F("AT+CWMODE_CUR=2\r\n"));
             delay(ESPWriteDelay);
             readESP();
@@ -581,14 +712,11 @@ void loop() {
         if(dataESPLength>=5 && dataESPLength<=6){
             /* Find space split into pin and operation, then compile and send */
             int space = dataESP.indexOf(F(" "));
-            int pin=dataESP.substring(0,space).toInt();
+            short pin=dataESP.substring(0,space).toInt();
             bool operation=dataESP.substring(space,dataESP.length()-2).toInt();
             operation=!operation;
-            pinMode(pinmap(pin),OUTPUT);
             sensorFlag=false;
-            digitalWrite(pinmap(pin),operation); // do operation
-            pins[pin]=operation;// update internal DB
-            dumpToEEPROM();
+            setpin(pin,operation);
             String pinString;
             pinString.reserve(10);
             pinString=dataESP.substring(0,dataESP.length()-2);
@@ -596,26 +724,113 @@ void loop() {
         }
         else{
         /* Here pin status is retrived and send to the SSAL Core */
-            if(dataESP.indexOf(F("sensor"))==-1 && dataESP.indexOf(F("hwinfo"))==-1){
-                int pin=dataESP.substring(0,dataESP.length()-2).toInt(); 
-                String pinString;
-                pinString.reserve(10);
-                pinString=String(!pins[pin]);
-                sensorFlag=false;
-                sendData(id,pinString);
+          if(dataESP.indexOf(F("sensor"))!=-1){
+            sensorFlag=true;
+          }
+          else if(dataESP.indexOf(F("hwinfo"))!=-1){
+            String pinString;
+            pinString.reserve(5);
+            pinString=String(DOS);
+            sensorFlag=false;
+            sendData(id,pinString);
+          }
+          else if(dataESP.indexOf(F("relaycfg"))!=-1){
+            //relaycfg code
+            //relaycfg relayNo paircode
+            String pinString;
+            String data = dataESP.substring(0,dataESP.length()-2);
+            int space1 = data.indexOf(" ");
+            int space2 = data.indexOf(" ",space1+1);
+            if(space1==-1 || space2==-1){
+              if(space1!=-1){
+                int relayNo=data.substring(space1+1).toInt();
+                pinString=String(relaycfg[relayNo-1]);
+              }
+              else{
+                pinString=F("Error");
+              }
             }
             else{
-                if(dataESP.indexOf(F("sensor"))==-1){
-                    String pinString;
-                    pinString.reserve(5);
-                    pinString=String(DOS);
-                    sensorFlag=false;
-                    sendData(id,pinString);
+              int relayNo=data.substring(space1+1,space2).toInt();
+              uint8_t value=data.substring(space2+1).toInt();
+              if(relayNo>0 && relayNo<5){
+                short cmpval=relayMaptoInt(value);
+                if(cmpval>=0 && cmpval<=28){
+                  relaycfg[relayNo-1]=value;
+                  Serial.print(F("Relay "));
+                  Serial.print(relayNo);
+                  Serial.print(F(" "));
+                  Serial.println(value);
+                  relayConfDumpEEPROM();
+                  pinString=F("OK");
                 }
                 else{
-                    sensorFlag=true;
+                  pinString=F("Error value");
                 }
+              }
+              else{
+                pinString=F("Error relayNo");
+              }
             }
+            sendData(id,pinString);
+          }
+          else if(dataESP.indexOf(F("setrelay"))!=-1){
+            /*  setrelay relaynumber mode
+                ------modes------
+                0-> OFF
+                1-> ON GRID
+                2-> ON INV
+                -----------------
+            */
+            String pinString;
+            String data = dataESP.substring(0,dataESP.length()-2);
+            int space1 = data.indexOf(" ");
+            int space2 = data.indexOf(" ",space1+1);
+            if(space1==-1 || space2==-1){
+              pinString=F("Error");
+            }
+            else{
+              short relayNo=data.substring(space1+1,space2).toInt();
+              uint8_t value=data.substring(space2+1).toInt();
+              if(relayNo>0 && relayNo<5){
+                short pin1,pin2;
+                if(relayConfDecode(relaycfg[relayNo-1],&pin1,&pin2)){
+                  pinString="OK";
+                  if(value==0){
+                    setpin(pin1,1);
+                    setpin(pin2,1);
+                  }
+                  else if(value==1){
+                    setpin(pin1,0);
+                    setpin(pin2,1);
+                  }
+                  else if(value==2){
+                    setpin(pin1,0);
+                    setpin(pin2,0);
+                  }
+                  else{
+                    pinString=F("Error value");
+                  }
+                }
+                else{
+                  pinString=F("Error conf");
+                }
+              }
+              else{
+                pinString=F("Error relayNo");
+              }
+            }
+            sendData(id,pinString);
+          }
+          else{
+            //pin status
+            int pin=dataESP.substring(0,dataESP.length()-2).toInt(); 
+            String pinString;
+            pinString.reserve(10);
+            pinString=String(!pins[pin]);
+            sensorFlag=false;
+            sendData(id,pinString);
+          }
         }
       }
       else{
