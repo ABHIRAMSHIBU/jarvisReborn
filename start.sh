7052c476e5cb3685438fecc2ccd1c5b3844e651c
Abhiram Shibu <abhiramshibu1998@gmail.com>
2020-05-12 19:54:21 +0530

    influxDB: Throw less logs

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 echo "Project SSAL-OS docker container for influxdb is starting.. Please Wait.."
 echo "Port 8086 and 8083 will be listning"
 sudo docker container rm db
-sudo docker run -p 8086:8086 -p 8083:8083 -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro -v $PWD/influx.log:/var/log/influx.log -v $PWD/database:/var/lib/influxdb --name "db" influxdb -config /etc/influxdb/influxdb.conf --log-opt max-size=50m 
+sudo docker run -p 8086:8086 -p 8083:8083 -v /opt/influxdb/influxdb.conf:/etc/influxdb/influxdb.conf:ro -v /opt/influxdb/influx.log:/var/log/influx.log -v /opt/influxdb/database:/var/lib/influxdb --name "db" influxdb -config /etc/influxdb/influxdb.conf 
 if [ $? -ne 0 ]
 then
 	echo "Some problem occured, trying to start with name"
